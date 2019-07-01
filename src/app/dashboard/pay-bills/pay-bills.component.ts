9deb4f8a278ab7a3f94c5798a7bec072b95e0977
feyex <feyisewaawotedu@gmail.com>
2019-07-01 17:44:17 +0100

    profile,history integrated

--- src/app/dashboard/pay-bills/pay-bills.component.ts
+++ src/app/dashboard/pay-bills/pay-bills.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserAuthService } from '../../auth/user-auth.service';
+
+
 
 @Component({
   selector: 'app-pay-bills',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pay-bills.component.css']
 })
 export class PayBillsComponent implements OnInit {
+  user: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private auth:  UserAuthService,
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params=>{
+      this.auth.gettransaction(params['id'])
+        .subscribe(res =>{
+          this.user = res;
+          console.log('users',this.user.meter_number)
+        })
+    })
   }
 
 }
+
