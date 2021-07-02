c9a59638ea652f99a34db0a26349ee31356a3829
feyisewa <feyisewaawotedu@gmail.com>
2021-07-02 10:23:48 +0100

    updated

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
-const cors = require('cors');
 
-app.use(cors());
-app.use(express.static('./dist/client-app'));
+const app = express();
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, './../dist/client-app/index.html'));
-    console.log('path', path.join(__dirname, './../dist/client-app/index.html'))
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/client-app'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/client-app/index.html'));
 });
 
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 4200);
